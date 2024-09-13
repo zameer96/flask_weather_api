0f42315be4bbc79078f3edc11573ffae4d5d14c1
zameerkhan <200546819@student.georgianc.on.ca>
2024-09-13 10:46:20 -0400

    [Fix] set app dir for flask run

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 from flask import Flask
 from flask_migrate import Migrate
 from config import Config
 from weather_app.db import db as weather_db
 from weather_app.routes import routes as weather_routes
 
-def create_app(app_name):
+def create_app(app_name=__name__):
     app = Flask(app_name)
     app.config.from_object(Config)
 
@@ -19,7 +24,7 @@ def create_app(app_name):
     
     return app
 
-app = create_app(__name__)
+app = create_app()
 
 if __name__ == '__main__':        
     app.run(debug=True)
